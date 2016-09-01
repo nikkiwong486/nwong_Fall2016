5b6e2e2fb4b2e532e035aa64d6df096c6e7b1688
nikkiwong486 <18wongn27@millshighsch.org>
2016-09-01 08:54:27 -0700

    more functions

--- Calculate Library/Calculate.java
+++ Calculate Library/Calculate.java
@@ -6,12 +6,25 @@ public class Calculate {
 //input: #
 //	output:#
 	
-	public static double square(double number) {
-		double answer;
+	public static int square(int number) {
+		int answer;
 		answer=number*number;
 		return answer;
-		
-		
+	}
+	public static int cube(int number) {
+		int answer;
+		answer=number*number*number;
+		return answer;
+	}
+	public static int average(int number) {
+		int answer;
+		answer=(number*number)/2;
+		return answer;
+	}
+	public static double toDegree(double number) {
+		double answer;
+		answer=(180*number)/3.14159;
+		return answer;
 	}
 
 }
