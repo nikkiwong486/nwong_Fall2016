5977f9d2772af2886fa8f4e3a5e40b9e0417b2ea
nikkiwong486 <18wongn27@millshighsch.org>
2016-10-25 08:29:47 -0700

    everything

--- Magpie Chatbot Lab/src/IntroductionToTheMagpieClass.java
+++
@@ -1,9 +0,0 @@
-
-public class IntroductionToTheMagpieClass {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
--- SplitStringLab/src/SplitStringLab.java
+++
@@ -1,9 +0,0 @@
-
-public class SplitStringLab {
-
-	public static void main(String[] args) {
-		askdlfjsaf;
-
-	}
-
-}
