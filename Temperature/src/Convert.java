169c6eeabdc2b891870d8ffa3f2dc97a7aedf94a
nikkiwong486 <18wongn27@millshighsch.org>
2016-10-25 09:33:58 -0700

    restart 10.25.16

---
+++ Temperature/src/Convert.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+
+/**
+ * @author Nikki Wong August 30, 2016
+ *
+ */
+public class Convert {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			int F=212;
+			double C;
+			C=(5.0/9.0)*(F-32);
+			System.out.println(C );
+	}
+
+}
