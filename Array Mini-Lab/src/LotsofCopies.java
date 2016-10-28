581b208d584e90fcfc193c8f3b83b68abaafc050
nikkiwong486 <18wongn27@millshighsch.org>
2016-10-28 08:47:33 -0700

    Array Mini Lab

---
+++ Array Mini-Lab/src/LotsofCopies.java
@@ -0,0 +1,18 @@
+
+public class LotsofCopies
+{
+	public static void main (String[] args)
+	{	
+			int num = 7;
+			String strMain = "APCS";
+			int [] arrMain = {1, 2, 3, 4, 5};
+		}
+}
+	public static void changeMe (int x, String str, int [] arr)
+	{
+		x = 7;
+		str = "blah";
+		for (int i = 0; i <= 4; i++){
+		arr[i] = i;
+		}
+}
