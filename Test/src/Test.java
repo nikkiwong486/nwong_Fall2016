2653cdba059971b701b90f78db85bd0d2c8d9f64
nikkiwong486 <18wongn27@millshighsch.org>
2016-12-16 00:31:30 -0800

    Worksheet

---
+++ Test/src/Test.java
@@ -0,0 +1,13 @@
+
+public class Test {
+
+	public static void main(String[] s){
+		for(int i=2;i<4;i++){
+			System.out.print("I really\n");
+			for(int j=5;j>2;j--){
+				System.out.print("love CS\n");
+			}
+			System.out.print("yes I do");
+		}
+	}
+}
